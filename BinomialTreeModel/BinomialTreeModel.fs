de8403cfe22f9d1cdced2f631dce548032d1cfab
teramonagi <teramonagi@gmail.com>
2014-03-05 21:11:10 +0900

    Refactored code

--- BinomialTreeModel/BinomialTreeModel.fs
+++ BinomialTreeModel/BinomialTreeModel.fs
@@ -6,39 +6,37 @@ type BinaryTree =
     | Node of float*BinaryTree*BinaryTree
 //Option type with payoff
 type OptionType =
-    | American of (float -> float)
-    | European of (float -> float)
-    member this.Payoff = 
-        match this with 
-            | American payoff -> payoff
-            | European payoff -> payoff
+    | American
+    | European
 //Make binary tree
-let makeTree s0 u d n=
+let makeTree s0 u d n =
+    let calc (v,i) = s0*(u**float(v))*(d**float(i))
     let rec makeTreeInner m = 
         match m with
             | 0 ->
-                [| for i in 0..n -> Node(s0*(u**float(n-i))*(d**float(i)), Empty, Empty) |]
+                [| for i in 0..n -> Node(calc(n-i, i), Empty, Empty) |]
             | _ -> 
                 let childs = makeTreeInner (m-1)
-                [| for i in 0..(n-m) -> Node(s0*(u**float(n-m-i))*(d**float(i)), childs.[i], childs.[i+1]) |]
+                [| for i in 0..(n-m) -> Node(calc(n-m-i, i), childs.[i], childs.[i+1]) |]   
     (makeTreeInner n).[0]
+
 //Option Pricer
-let evalOption (option:OptionType) s0 r T vol n =
+let evalOption optionType payoff s0 r T vol n =
     let dt = T/(float n)
     let u = exp(vol*sqrt(dt))
     let d = 1.0/u
     let tree = makeTree s0 u d n
     let p = (exp(r*dt)-d)/(u-d)
-    let rec evalOptionInner (option:OptionType) tree =
+    let rec evalOptionInner tree =
         match tree with 
             | Empty -> 0.0
-            | Node(value, Empty, Empty) -> option.Payoff value
+            | Node(value, Empty, Empty) -> payoff value
             | Node(value, lNode, rNode) -> 
-                let lValue = evalOptionInner option lNode                
-                let rValue = evalOptionInner option rNode
+                let lValue = evalOptionInner lNode                
+                let rValue = evalOptionInner rNode
                 let currentValue = (exp(-r*dt)*(p*lValue+(1.0-p)*rValue))
-                match option with 
-                    | American payoff -> max (payoff value) currentValue
-                    | European _      -> currentValue
-    evalOptionInner option tree
+                match optionType with 
+                    | American -> max (payoff value) currentValue
+                    | European -> currentValue
+    evalOptionInner tree
 
